038fca7116e6ec5bf12af6eb7d5dd71ffd71d29a
nalin577 <miachelwattson123@gmail.com>
2021-04-20 11:26:41 +0530

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          appBar: AppBar(
+            title: Center(
+              child: Text('Magic 8 Ball'),
+            ),
+            backgroundColor: Colors.teal,
+          ),
+          body: MagicPage(),
+        ),
       ),
     );
+
+class MagicPage extends StatefulWidget {
+  @override
+  _MagicPageState createState() => _MagicPageState();
+}
+
+class _MagicPageState extends State<MagicPage> {
+  int num = 1;
+  void ChangeAnswer() {
+    setState(() {
+      num = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    ChangeAnswer();
+                  },
+                  child: Image.asset('images/ball$num.png')))
+        ],
+      ),
+    );
+  }
+}
